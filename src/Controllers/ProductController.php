489d2503b37a0bcf32d5278a7f49a3d4c08392d3
fahlanlogic <alanfahdi@gmail.com>
2026-07-16 18:10:42 +0700

    feat: class response

--- src/Controllers/ProductController.php
+++ src/Controllers/ProductController.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Controllers;
 
+use App\Response;
 use App\Services\ProductService;
 
 class ProductController {
@@ -13,18 +14,12 @@ class ProductController {
     public function index(): void {
         $products = $this->product_service->getAll();
 
-        echo json_encode([
-            "message" => "Data fetched",
-            "data" => $products
-        ]);
+        Response::json($products);
     }
 
     public function store(): void {
         $product = $this->product_service->create();
 
-        echo json_encode([
-            "message" => "Operation success",
-            "data" => $product
-        ]);
+        Response::json($product);
     }
 }
---
+++ src/Response.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App;
+
+final class Response {
+    public static function json(array $data, int $statusCode = 200): void {
+        $method = $_SERVER['REQUEST_METHOD'];
+        header('Content-type: application/json');
+        http_response_code($statusCode);
+
+        if ($statusCode >= 399) {
+            echo json_encode([
+                'message' => 'Operation failed',
+                'data' => null
+            ]);
+
+            exit;
+        }
+
+        if ($method === 'GET') {
+            echo json_encode([
+                'message' => 'Data fetched',
+                'data' => $data
+            ]);
+        }
+        if ($method === 'POST' || $method === 'PATCH' || $method === 'PUT' || $method === 'DELETE') {
+            echo json_encode([
+                'message' => 'Operation success',
+                'data' => $data
+            ]);
+        }
+
+        exit;
+    }
+}
